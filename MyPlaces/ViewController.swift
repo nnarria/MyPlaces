f951eb2641c078cb08ed4d27721637ecc6179351
Nicolás Narria <nnarria@gmail.com>
2016-07-20 11:29:01 -0400

    Initial Commit

---
+++ MyPlaces/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by Nicolás Narria on 7/20/16.
+//  Copyright © 2016 Nicolás Narria. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
